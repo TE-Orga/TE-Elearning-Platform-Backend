84f3cbc17ff4a62033332025421faadb5d044e83
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2025-01-29 18:21:12 +0100

    finish the database seed

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if ($user && Hash::check($request->password, $user->password)) {
+        return $user->createToken('YourAppName')->plainTextToken;
+    }
+
+    return response()->json(['message' => 'Unauthorized'], 401);
 });
